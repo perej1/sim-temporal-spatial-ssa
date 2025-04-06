da3f395127346700836e8d7456f6c441eb3bb05b
perej1 <jaakko.pere@aalto.fi>
2025-04-06 17:07:14 +0300

    Include time grid

--- gen-spatial-points.R
+++ gen-spatial-points.R
@@ -1,6 +1,10 @@
 library(tmap)
 library(dplyr)
 
+# Global parameters
+n_spat <- 5L
+n_time <- 3L
+country_name <- "italy"
 
 #' Get country bounding box and polygon
 #'
@@ -19,8 +23,8 @@ get_country <- function(country) {
   }
 
   sf_country <- rnaturalearth::ne_countries(returnclass = "sf",
-                                            scale = "medium") %>%
-    dplyr::filter(name == name_polygon) %>%
+                                            scale = "medium",
+                                            country = name_polygon) %>%
     sf::st_transform(crs = 4326)
 
   bounds <- rjson::fromJSON(file = "data/bounding-boxes.json")[[name_box]][[2]]
@@ -29,80 +33,77 @@ get_country <- function(country) {
 }
 
 
-#' Generate uniform sample of points from a bounding box
+#' Generate uniform sample of spatial locations from a country
 #'
 #' Points that do not lie on the country polygon are filtered out. Thus, the
 #'  effective sample size is <= n.
 #'
-#' @param n Sample size
+#' @param n_spat Number of spatial locations
 #' @param sf_country Country polygon
 #' @param bounds Country bounding box
 #'
-#' @returns sf object with coordinated but with 0 features
-gen_unif_coords_country <- function(n, sf_country, bounds) {
+#' @returns sf object with coordinates and 0 features
+gen_unif_coords_country <- function(n_spat, sf_country, bounds) {
 
-  lat <- stats::runif(n, bounds["lat_min"], bounds["lat_max"])
-  lon <- stats::runif(n, bounds["lon_min"], bounds["lon_max"])
+  lat <- stats::runif(n_spat, bounds["lat_min"], bounds["lat_max"])
+  lon <- stats::runif(n_spat, bounds["lon_min"], bounds["lon_max"])
 
   data <- tibble::tibble(lat = lat, lon = lon) %>%
-    sf::st_as_sf(coords = c("lon", "lat"), crs = 4326)
+    sf::st_as_sf(coords = c("lon", "lat"), crs = 4326) # Original was sf
 
   data %>%
     sf::st_filter(sf_country, .predicate = sf::st_within)
 }
 
 
-#' Generate uniform sample from a box
+#' Generate uniform sample of spatial locations from a rectangle
 #'
-#' @param n Sample size
+#' @param n_spat Number of spatial locations
 #' @param left Left border
 #' @param right Right border
 #' @param low Lower border
 #' @param up Upper border
 #'
 #' @returns sf object with coordinated but with 0 features
-gen_unif_coords_box <- function(n, left = 0, right = 1, low = 0, up = 1) {
-  x <- stats::runif(n, left, right)
-  y <- stats::runif(n, low, up)
+gen_unif_coords_box <- function(n_spat, left = 0, right = 1, low = 0, up = 1) {
+  x <- stats::runif(n_spat, left, right)
+  y <- stats::runif(n_spat, low, up)
   tibble::tibble(x = x, y = y) %>%
     sf::st_as_sf(coords = c("x", "y"))
 }
 
 
-#' Generate sample from a distribution on a polygon
+#' Generate sample of spatial locations from a distribution on a country polygon
 #'
-#' @param n Sample size
+#' @param n_spat Number of spatial locations
 #' @param gen_style_coords Function for generating samples
 #' @param ... Optional parameters for the function gen_style_coords
 #'
 #' @returns sf object with coordinated but with 0 features
-gen_coords <- function(n = 100, gen_style_coords, ...) {
-  coords <- gen_style_coords(n, ...)
-  while (nrow(coords) < n) {
-    coords <- rbind(coords, gen_style_coords(n, ...))
+gen_coords <- function(n_spat = 100, gen_style_coords, ...) {
+  coords <- gen_style_coords(n_spat, ...)
+  while (nrow(coords) < n_spat) {
+    coords <- rbind(coords, gen_style_coords(n_spat, ...))
   }
-  dplyr::slice(coords, 1:n)
+  coords %>%
+    dplyr::slice(1:n_spat)
 }
 
 
 # Example with a country
-n <- 20
-country <- get_country("italy")
+country <- get_country(country_name)
 
-coords <- gen_coords(n, gen_unif_coords_country, country$sf_country,
-                     country$bounding_box)
-data <- coords %>%
-  dplyr::mutate(a = 1:n)
+t <- 0:(n_time - 1)
+
+coords <- gen_coords(n_spat, gen_unif_coords_country, country$sf_country,
+                     country$bounding_box) %>%
+  replicate(n_time, ., simplify = FALSE) %>%
+  bind_rows() %>%
+  dplyr::mutate(time = rep(t, each = n_spat), a = seq_len(nrow(.))) %>%
+  sftime::st_sftime(sf_column_name = "geometry", time_column_name = "time")
 
 tm_shape(country$sf_country) +
   tm_polygons() +
-  tm_shape(data) +
-  tm_bubbles(col = "black", fill = "red", size = 0.5)
-
-
-# Example with a box
-coords <- gen_coords(n, gen_unif_coords_box, -1, 1, -1, 1)
-data <- coords %>%
-  dplyr::mutate(a = 1:n)
-
-plot(data)
+  tm_shape(coords) +
+  tm_facets(by = "time") +
+  tm_bubbles(col = "black", fill = "a", size = 0.5)
