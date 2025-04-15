3a04b2c906e3dcb8f03f7eea4de1797523eb5b43
perej1 <jaakko.pere@aalto.fi>
2025-04-15 23:55:12 +0300

    In the bash script do simulations with default params
    Just to have a working example.

--- sim-batch.sh
+++ sim-batch.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-Rscript gen-spatial-points.R --n_spat 10 --n_time 10 --m 1 --x_blocks 33:33:34 --y_blocks 33:33:34 --time_blocks 33:33:34 --setting 1 --file results/test10.txt
-Rscript gen-spatial-points.R --n_spat 100 --n_time 100 --m 1 --x_blocks 33:33:34 --y_blocks 33:33:34 --time_blocks 33:33:34 --setting 1 --file results/test100.txt
-Rscript gen-spatial-points.R --n_spat 1000 --n_time 1000 --m 1 --x_blocks 33:33:34 --y_blocks 33:33:34 --time_blocks 33:33:34 --setting 1 --file results/test1000.txt
+Rscript simulate-setting.R
