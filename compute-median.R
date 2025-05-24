04e81ba988bffb034ae5877702f7c4bcf91d19a4
perej1 <jaakko.pere@aalto.fi>
2025-05-25 01:19:44 +0300

    Compute avg lambda for each setting

--- compute-median.R
+++ compute-median.R
@@ -3,14 +3,27 @@
 library(dplyr)
 
 args <- readr::read_csv("sim-args.csv", col_types = "ciiiccclii")
-quantiles <- tibble::tibble(stationary_lower_quartile = rep(NA, nrow(args)),
-                            stationary_median = NA,
-                            stationary_upper_quartile = NA,
-                            nonstationary_lower_quartile = NA,
-                            nonstationary_median = NA,
-                            nonstationary_upper_quartile = NA)
 
+# Initialize tibbles for statistics
+perf_quantiles <- tibble::tibble(
+  stationary_lower_quartile = rep(NA, nrow(args)),
+  stationary_median = NA,
+  stationary_upper_quartile = NA,
+  nonstationary_lower_quartile = NA,
+  nonstationary_median = NA,
+  nonstationary_upper_quartile = NA
+)
+
+lambda_avg <- tibble(
+  f1 = rep(NA, nrow(args)),
+  f2 = NA,
+  f3 = NA,
+  f4 = NA
+)
+
+# Compute statistics for each setting
 for (i in seq_len(nrow(args))) {
+
   arg <- args[i, ]
   filename <- filename <- stringr::str_c(
     "latent_", arg$latent,
@@ -24,10 +37,14 @@ for (i in seq_len(nrow(args))) {
     "_seed_spatial_", arg$seed_spatial,
     "_seed_sim_", arg$seed_sim, ".csv"
   )
-  q_i <- read.csv(stringr::str_c("results/perf/", filename)) %>%
+
+  # Compute 1st, 2nd and 3rd quartiles for performance
+  q_i <- readr::read_csv(stringr::str_c("results/perf/", filename),
+                         col_types = "dd") %>%
     apply(2, quantile, probs = c(0.25, 0.5, 0.75)) %>%
     tibble::as_tibble()
-  quantiles[i, ] <- tibble::tibble(
+
+  perf_quantiles[i, ] <- tibble::tibble(
     stationary_lower_quartile = q_i$stationary[1],
     stationary_median = q_i$stationary[2],
     stationary_upper_quartile = q_i$stationary[3],
@@ -35,7 +52,17 @@ for (i in seq_len(nrow(args))) {
     nonstationary_median = q_i$nonstationary[2],
     nonstationary_upper_quartile = q_i$nonstationary[3]
   )
+
+  # Compute average eigenvalue
+  lambda_avg[i, ] <- readr::read_csv(stringr::str_c("results/eigen/", filename),
+                                     col_types = "dddd") %>%
+    colMeans() %>%
+    as.list() %>%
+    tibble::as_tibble()
 }
 
-bind_cols(args, quantiles) %>%
+bind_cols(args, perf_quantiles) %>%
   readr::write_csv("results/perf-quantiles.csv")
+
+bind_cols(args, lambda_avg) %>%
+  readr::write_csv("results/lambda-avg.csv")
