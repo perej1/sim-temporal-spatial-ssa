f663ff44dc62d6b34bb1aebadf7dc4b101b11a32
perej1 <jaakko.pere@aalto.fi>
2025-06-02 21:29:10 +0300

    Compute stats corresponding to the current settings

--- compute-stats.R
+++ compute-stats.R
@@ -6,7 +6,7 @@
 
 library(dplyr)
 
-args <- readr::read_csv("sim-args.csv", col_types = "ciiiccclii")
+args <- readr::read_csv("sim-args.csv", col_types = "cciiicccliii")
 
 # Initialize tibbles for statistics
 perf_quantiles <- tibble::tibble(
@@ -18,28 +18,39 @@ perf_quantiles <- tibble::tibble(
   nonstationary_upper_quartile = NA
 )
 
-lambda_avg <- tibble(
+lambda_avg_osc <- tibble(
   f1 = rep(NA, nrow(args)),
   f2 = NA,
   f3 = NA,
   f4 = NA
 )
 
+lambda_avg_spacetime <- tibble(
+  f1 = rep(NA, nrow(args)),
+  f2 = NA,
+  f3 = NA,
+  f4 = NA,
+  f5 = NA,
+  f6 = NA,
+  f7 = NA,
+  f8 = NA
+)
+
+
 # Compute statistics for each setting
 for (i in seq_len(nrow(args))) {
 
   arg <- args[i, ]
-  filename <- filename <- stringr::str_c(
-    "latent_", arg$latent,
+  filename <- stringr::str_c(
+    "mu_", arg$mu,
+    "_epsilon_", arg$epsilon,
     "_n_spatial_", arg$n_spatial,
     "_n_time_", arg$n_time,
     "_m_", arg$m,
     "_x_blocks_", arg$x_blocks,
     "_y_blocks_", arg$y_blocks,
     "_time_blocks_", arg$time_blocks,
-    "_random_eigenvect_", arg$random_eigenvect,
-    "_seed_spatial_", arg$seed_spatial,
-    "_seed_sim_", arg$seed_sim, ".csv"
+    "_random_eigenvect_", arg$random_eigenvect, ".csv"
   )
 
   # Compute 1st, 2nd and 3rd quartiles for performance
@@ -58,15 +69,29 @@ for (i in seq_len(nrow(args))) {
   )
 
   # Compute componentwise average eigenvalue
-  lambda_avg[i, ] <- readr::read_csv(stringr::str_c("results/eigen/", filename),
-                                     col_types = "dddd") %>%
-    colMeans() %>%
-    as.list() %>%
-    tibble::as_tibble()
+  path <- "results/eigen/"
+  if (arg$mu == "oscillating") {
+    lambda_avg_osc[i, ] <- readr::read_csv(stringr::str_c(path, filename),
+                                           col_types = "dddd") %>%
+      colMeans() %>%
+      as.list() %>%
+      tibble::as_tibble()
+  } else if (arg$mu == "spacetime") {
+    lambda_avg_spacetime[i, ] <- readr::read_csv(stringr::str_c(path, filename),
+                                                 col_types = "dddddddd") %>%
+      colMeans() %>%
+      as.list() %>%
+      tibble::as_tibble()
+  }
 }
 
 bind_cols(args, perf_quantiles) %>%
   readr::write_csv("results/perf-quantiles.csv")
 
-bind_cols(args, lambda_avg) %>%
-  readr::write_csv("results/lambda-avg.csv")
+bind_cols(args, lambda_avg_osc) %>%
+  tidyr::drop_na() %>%
+  readr::write_csv("results/lambda-avg-osc.csv")
+
+bind_cols(args, lambda_avg_spacetime) %>%
+  tidyr::drop_na() %>%
+  readr::write_csv("results/lambda-avg-spacetime.csv")
